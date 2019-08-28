e52a9147c984361189b2bed876e57487309f7cc4
Wayne Rambo <wayne@fanzo.me>
2019-08-27 18:39:31 -0700

    Cleanup and getting user to return in request.

--- cloud/main.js
+++ cloud/main.js
@@ -17,11 +17,9 @@ Parse.Cloud.define('createToken', function(req, res) {
 });
 // Create the Cloud Function
 
-Parse.Cloud.define("findUser3", async req => {
+Parse.Cloud.define("sendCode", async req => {
   var phoneNumber = req.params.phoneNumber;
   phoneNumber = phoneNumber.replace(/\D/g, '');
-
-  console.log("Incoming phone number is: " + phoneNumber)
 
   var userQuery = new Parse.Query(Parse.User);
   userQuery.equalTo('username', phoneNumber);
@@ -30,21 +28,17 @@ Parse.Cloud.define("findUser3", async req => {
   var num = Math.floor(Math.random() * (max - min + 1)) + min;
 
   var user = await userQuery.first({ useMasterKey: true });
-  console.log("Did we find a user, user is: " + user);
+  
   if(user) {
-    console.log("Found a user, user is: " + user);
+    console.log("Found a user, user is: " + user.getUsername());
     user.setPassword(secretPasswordToken + num);
-    // user.set("language", "en");
     user.save(null, { useMasterKey: true }).then(function() {
-        // sendCodeSms(phoneNumber, num, "en");
-        console.log("About to send an SMS to number:" + phoneNumber)
-        console.log("About to text number: " + phoneNumber)
         twilioClient.messages.create({
-          body: 'Hello from Benji-api!',
-          from: '+12012560616', // From a valid Twilio number
-          to: phoneNumber  // Text this number
+          body: 'Your phone number was just used on the Benji App. Your auth code is: ' + num,
+          from: '+12012560616', 
+          to: phoneNumber  
         })
-        console.log("Sent the SMS")
+        console.log("Sent the existing user SMS")
     })
   } else {
     console.log("Did not find a user, create and return it");
@@ -53,18 +47,14 @@ Parse.Cloud.define("findUser3", async req => {
     newUser.setPassword(secretPasswordToken + phoneNumber);
     newUser.set("language", "en");
     newUser.setACL({});
-    await newUser.save(); //This is the line you need to change
+    await newUser.save(); 
 
-    console.log("About to send a SMS")
-    console.log("About to text number: " + phoneNumber)
     twilioClient.messages.create({
-      body: 'Hello from Benji-api!',
-      from: '+12012560616', // From a valid Twilio number
-      to: phoneNumber  // Text this number
+      body: 'Your phone number was just used on the Benji App. Your auth code is: ' + num,
+      from: '+12012560616', 
+      to: phoneNumber  
     })
-    console.log("Sent the SMS")
-    //If this is not commented out, fails with:
-    //Error: Cannot create a pointer to an unsaved ParseObject
+    console.log("Sent the new user SMS")
     user = newUser;
   }
   console.log("about to return the user");
@@ -72,58 +62,58 @@ Parse.Cloud.define("findUser3", async req => {
 });
 
 
-Parse.Cloud.define("sendCode", async req => {
-  console.log("in SendCode")
-  var phoneNumber = req.params.phoneNumber;
-  phoneNumber = phoneNumber.replace(/\D/g, '');
-  var lang = req.params.language;
+// Parse.Cloud.define("sendCode", async req => {
+//   console.log("in SendCode")
+//   var phoneNumber = req.params.phoneNumber;
+//   phoneNumber = phoneNumber.replace(/\D/g, '');
+//   var lang = req.params.language;
 
-  if(lang !== undefined && languages.indexOf(lang) != -1) {
-    language = lang;
-  }
-  if (!phoneNumber || (phoneNumber.length != 10 && phoneNumber.length != 11)) return res.error('Invalid Parameters');
-  var query = new Parse.Query(Parse.User);
-  console.log("about to look up phone number: " + phoneNumber);
-  query.equalTo('username', phoneNumber + "");
-  const foundUser = await query.first();
+//   if(lang !== undefined && languages.indexOf(lang) != -1) {
+//     language = lang;
+//   }
+//   if (!phoneNumber || (phoneNumber.length != 10 && phoneNumber.length != 11)) return res.error('Invalid Parameters');
+//   var query = new Parse.Query(Parse.User);
+//   console.log("about to look up phone number: " + phoneNumber);
+//   query.equalTo('username', phoneNumber + "");
+//   const foundUser = await query.first();
 
-  var min = 1000; var max = 9999;
-  var num = Math.floor(Math.random() * (max - min + 1)) + min;
-  if (foundUser){
-    console.log("found a result")
-    result.setPassword(secretPasswordToken + num);
-    result.set("language", "en");
-    result.save().then(function() {
-      console.log("About to text number: " + phoneNumber)
-      twilioClient.messages.create({
-        body: 'Hello from Benji-api! Your login code is: ' + num,
-        from: '+12012560616', // From a valid Twilio number
-        to: phoneNumber  // Text this number
-      })
-      // .then((message) => console.log(message.sid));
-      //   sendCodeSms(phoneNumber, num, language);
-    })
-  } else {
-    console.log("did not find a result")
-    var user = new Parse.User();
-    user.setUsername(phoneNumber);
-    user.setPassword(secretPasswordToken + num);
-    user.set("language", "en");
-    user.setACL({});
-    user.save().then(function(a) {
-      // sendCodeSms(phoneNumber, num, language);
-      console.log("About to text number: " + phoneNumber)
-      twilioClient.messages.create({
-        body: 'Hello from Benji-api! Your login code is: ' + num,
-        from: '+12012560616', // From a valid Twilio number
-        to: phoneNumber  // Text this number
-      })
-    });
-    foundUser = User
-  }  
+//   var min = 1000; var max = 9999;
+//   var num = Math.floor(Math.random() * (max - min + 1)) + min;
+//   if (foundUser){
+//     console.log("found a result")
+//     result.setPassword(secretPasswordToken + num);
+//     result.set("language", "en");
+//     result.save().then(function() {
+//       console.log("About to text number: " + phoneNumber)
+//       twilioClient.messages.create({
+//         body: 'Hello from Benji-api! Your login code is: ' + num,
+//         from: '+12012560616', // From a valid Twilio number
+//         to: phoneNumber  // Text this number
+//       })
+//       // .then((message) => console.log(message.sid));
+//       //   sendCodeSms(phoneNumber, num, language);
+//     })
+//   } else {
+//     console.log("did not find a result")
+//     var user = new Parse.User();
+//     user.setUsername(phoneNumber);
+//     user.setPassword(secretPasswordToken + num);
+//     user.set("language", "en");
+//     user.setACL({});
+//     user.save().then(function(a) {
+//       // sendCodeSms(phoneNumber, num, language);
+//       console.log("About to text number: " + phoneNumber)
+//       twilioClient.messages.create({
+//         body: 'Hello from Benji-api! Your login code is: ' + num,
+//         from: '+12012560616', // From a valid Twilio number
+//         to: phoneNumber  // Text this number
+//       })
+//     });
+//     foundUser = User
+//   }  
 
-  return foundUser
-})
+//   return foundUser
+// })
 
 Parse.Cloud.define("login", function(req, res) {
 
