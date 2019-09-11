f155d122b334232953fc4d3afcccf24a0c0c1502
Wayne Rambo <wayne@fanzo.me>
2019-09-10 19:21:06 -0700

    Wiring up dashboard to api

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://benji-api.herokuapp.com',  // Don't forget to change to https if needed
+  appId: process.env.APP_ID || 'BenjiApp',
+  masterKey: process.env.MASTER_KEY || 'theStupidMasterKeyThatShouldBeSecret', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'https://benji-api.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
